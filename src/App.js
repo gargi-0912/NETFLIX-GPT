54641e7554ba2974705dc34f38da3558ee23d333
gargi_0912 <gargigupta09122003@gmail.com>
2024-03-12 19:12:18 +0530

    signin/signup/login/header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
-import './App.css';
-
+import Body from './components/Body';
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-   Hlo everyone! Let's build Netflix  gpt
-    </div>
+    <Body/>
   );
 } 
 
