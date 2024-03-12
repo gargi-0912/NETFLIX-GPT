38cb80391478a2976d741c1dfedd04023377c218
gargi_0912 <gargigupta09122003@gmail.com>
2024-03-12 14:40:43 +0530

    cra-tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl font-bold text-green-800">
+   Hlo everyone! Let's build Netflix  gpt
     </div>
   );
-}
+} 
 
 export default App;
